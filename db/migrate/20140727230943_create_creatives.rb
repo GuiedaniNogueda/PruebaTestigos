60c054adc32df67218848facf0c12150d544c250
j0u135 <j0u135.uam@gmail.com>
2014-07-27 18:11:21 -0500

    Creación del objeto Creativo

---
+++ db/migrate/20140727230943_create_creatives.rb
@@ -0,0 +1,10 @@
+class CreateCreatives < ActiveRecord::Migration
+  def change
+    create_table :creatives do |t|
+      t.string :creative_name
+      t.integer :order_id
+
+      t.timestamps
+    end
+  end
+end
